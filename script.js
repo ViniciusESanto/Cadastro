14147a7357f945ace90a045c9d0172660173600a
Vinicius E Santo <viniciusesant@gmail.com>
2024-04-26 16:36:44 -0300

    exercicio

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#cpf').mask('000.000.000-00')
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cep').mask('00.000-000')
+})
+
+$('#button').click(function(){
+    alert("Seu cadastro foi realizado com sucesso!")
+})
